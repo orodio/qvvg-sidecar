27ac17510d7843eefa466765ced0a8b44ead0aff
James Hunter <github@qv.vg>
2019-10-30 16:49:58 -0700

    named functions for better perf debug

--- src/main.test.js
+++
@@ -1,6 +0,0 @@
-import {Domain, label, withName} from '@qvvg/sos'
-import {withSidecar} from './main.js'
-
-const counter = Domain([label('counter'), withName()])
-
-test('withSidecar', async () => {})
--- src/with-sidecar.js
+++ src/with-sidecar.js
@@ -1,46 +1,48 @@
 import React from 'react'
 import {Domain} from '@qvvg/sos'
 
-export const withSidecar = (mods = [], opts = {}) => Comp => {
-  const Sidecar = Domain(mods)
+export function withSidecar(mods = [], opts = {}) {
+  return function hoc(Comp) {
+    const Sidecar = Domain(mods)
 
-  class WithSidecar extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {}
-      this.setProps = (props = {}) => {
-        if (typeof props !== 'object')
-          throw new Error(`sidecar setProps(value) -- value must be an object`)
+    class WithSidecar extends React.Component {
+      constructor(props) {
+        super(props)
+        this.state = {}
+        this.setProps = function setProps(props = {}) {
+          if (typeof props !== 'object')
+            throw new Error(`sidecar setProps(value) -- value must be an object`)
 
-        setTimeout(() => this.mount && this.setState(props), 0)
-        return props
+          setTimeout(() => this.mount && this.setState(props), 0)
+          return props
+        }
+        this.$sidecar = Sidecar.init(props, {
+          ...opts,
+          inject: {setProps: this.setProps, ...(opts.inject || {})},
+        })
       }
-      this.$sidecar = Sidecar.init(props, {
-        ...opts,
-        inject: {setProps: this.setProps, ...(opts.inject || {})},
-      })
-    }
 
-    componentDidMount() {
-      this.mount = true
-    }
-
-    componentWillUnmount() {
-      this.mount = false
-      Domain.stop(this.$sidecar, 'Unmounted Coupled React Component')
-    }
-
-    render() {
-      if (this.$sidecar == null) return null
-      const label = Sidecar.node.label
-      const sidecarProps = {
-        [label + 'Sidecar']: Sidecar,
-        [label + 'Self']: () => this.$sidecar,
+      componentDidMount() {
+        this.mount = true
       }
-      return <Comp {...sidecarProps} {...this.props} {...this.state} />
+
+      componentWillUnmount() {
+        this.mount = false
+        Domain.stop(this.$sidecar, 'Unmounted Coupled React Component')
+      }
+
+      render() {
+        if (this.$sidecar == null) return null
+        const label = Sidecar.node.label
+        const sidecarProps = {
+          [label + 'Sidecar']: Sidecar,
+          [label + 'Self']: () => this.$sidecar,
+        }
+        return <Comp {...sidecarProps} {...this.props} {...this.state} />
+      }
     }
+
+    WithSidecar.Sidecar = Sidecar
+    return WithSidecar
   }
-
-  WithSidecar.Sidecar = Sidecar
-  return WithSidecar
 }
